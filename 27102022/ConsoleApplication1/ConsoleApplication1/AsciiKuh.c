dcbcec48716a4bb5d6c61bcb93b1bc063c7263e4
Peter Braun <peterkauft@gmx.de>
2022-10-28 09:06:23 +0200

    cleanup

--- 27102022/ConsoleApplication1/ConsoleApplication1/AsciiKuh.c
+++ 27102022/ConsoleApplication1/ConsoleApplication1/AsciiKuh.c
@@ -8,6 +8,13 @@ int main()
     printf(" *  /\---/\  \n");
     printf("    ~~   ~~ \n");
 
+    int x, z = 7;
+    x = (23 + 7) / 10 - 5;
+    x = x + z;
+    z = (12 % x);
+    printf("Z: %d \n X: %d", z, x);
+
+
     getchar();
     return 0;
 }
